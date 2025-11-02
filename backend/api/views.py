0b036f0cc31fd34549415625c503a6cc89a8d67d
demolishpain88@gmail.com <demolishpain88@gmail.com>
2025-11-02 15:06:47 -0400

    Coneccion con la base de datos SupaBase en el backend y se agrego gitignore para los archivos temporales

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# backend/api/views.py
+from django.http import JsonResponse
+from django.db import connection
 
-# Create your views here.
+def health(request):
+    try:
+        with connection.cursor() as cur:
+            cur.execute("SELECT 1;")
+            row = cur.fetchone()
+        return JsonResponse({"ok": True, "db": "connected", "select_1": row[0]})
+    except Exception as e:
+        return JsonResponse({"ok": False, "error": str(e)}, status=500)
